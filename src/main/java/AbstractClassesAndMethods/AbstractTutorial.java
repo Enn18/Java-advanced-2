b18b386ab99872ee11a872bfc5f9a528ad44388b
Enn18 <ennennuste3@gmail.com>
2022-11-13 23:48:29 +0200

    AbstractTutorial

---
+++ src/main/java/AbstractClassesAndMethods/AbstractTutorial.java
@@ -0,0 +1,26 @@
+package AbstractClassesAndMethods;
+
+abstract class Dog {
+
+    public void bark() {
+        System.out.println("Bark!");
+    }
+
+    public abstract void poop();
+
+}
+class Chihuahua extends Dog {
+
+    public void poop() {
+        System.out.println("Dog pooped!");
+    }
+}
+
+public class AbstractTutorial {
+    public static void main(String[] args) {
+        Chihuahua c = new Chihuahua();
+
+        c.bark();
+        c.poop();
+    }
+}
